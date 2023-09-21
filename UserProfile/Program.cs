22b7abc526e6770749eb8e07d4aa5205fbf4da49
iZnoG0uD <zelesnac@gmail.com>
2023-09-21 23:26:53 +0400

    Добавьте файлы проекта.

---
+++ UserProfile/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace UserProfile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как Вас зовут? ");
+            var name = Console.ReadLine();
+            Console.Write("Ваш возвраст? ");
+            var age = int.Parse(Console.ReadLine());
+            Console.Write("Ваша дата рождения?");
+            var bDay = Console.ReadLine();
+
+            Console.WriteLine("\tВаши данные:\n \t Имя: {0} \n \t Возраст: {1} \n \t Дата Рождения : {2}", name, age, bDay);
+         }
+    }
+}
